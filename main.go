6f4cc8df3eff4d8e431bcf42ed8d709ad1b74110
generalmaks <schrinan@gmail.com>
2025-02-09 21:49:05 +0200

    Updated handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"lab_1/handlers"
+	"log"
 	"net"
+	"net/http"
 )
 
 const host = "localhost"
@@ -10,5 +12,20 @@ const port = "8795"
 
 func main() {
 	address := net.JoinHostPort(host, port)
-	fmt.Println(address)
+	for path, handler := range handlers.Handlers {
+		controller := func(response http.ResponseWriter, request *http.Request) {
+			for _, method := range handler.Methods {
+				if method == request.Method {
+					handler.Controller(response, request)
+					return
+				}
+				response.WriteHeader(http.StatusMethodNotAllowed)
+				message := "Method " + request.Method + " not allowed"
+				response.Write([]byte(message))
+			}
+		}
+		http.HandleFunc(path, controller)
+	}
+	fail := http.ListenAndServe(address, nil)
+	log.Fatal(fail)
 }
